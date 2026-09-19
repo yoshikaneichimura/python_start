4aa0efcc2628e35d72c5088fdc83a97e2ee3b627
{yoshikaneichimura} <{yoshikanemobile@gmail.com}>
2026-09-19 19:35:59 +0900

    python_start [Start]

---
+++ python.py
@@ -0,0 +1,25 @@
+# This is a comment.
+
+# print("Hello, World!")
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+# x = 5
+# y = "Hello, World!"
+
+# print("Hello World!")
+# print("Have a good day.")
+# print("Learning Python is fun!")
+
+# print("Hello"); print("How are you"); print("Bey bye!")
+
+# print("Hello World")
+# print("I am learning Python.")
+# print("It is awesome!")
+
+# print("This will work!")
+# print('This will also work!')
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")

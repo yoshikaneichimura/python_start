8de55a37f7674b52297b90fb742b2e811b5370af
{yoshikaneichimura} <{yoshikanemobile@gmail.com}>
2026-09-21 15:51:35 +0900

    変数 データ型 [Add]

--- python.py
+++ python.py
@@ -1,3 +1,6 @@
+import random
+
+"""構文"""
 # This is a comment.
 
 # print("Hello, World!")
@@ -7,6 +10,8 @@
 
 # x = 5
 # y = "Hello, World!"
+
+"""出力"""
 
 # print("Hello World!")
 # print("Have a good day.")
@@ -21,5 +26,190 @@
 # print("This will work!")
 # print('This will also work!')
 
-print("Hello World!", end=" ")
-print("I will print on the same line.")
+# print("Hello World!", end=" ")
+# print("I will print on the same line.")
+
+# print(3)
+# print(358)
+# print(50000)
+
+# print(3 + 3)
+# print(2 * 5)
+
+# print("I am", 35, "years old.")
+
+#print("Hello, World!") #This is a comment 
+
+# print("Cheers, Mate!")
+
+# This is comment
+# written in 
+# more than just one line
+
+# print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+# print("Hello World!")
+"""変数"""
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 4
+# x = "Sally"
+
+# print(x)
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(x); print(y); print(z)
+# print(type(x)); print(type(y)); print(type(z)) 
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# x = "John"
+# is the same as
+# x = 'John'
+
+# print(x)
+
+# a = 4
+# A = "Sally"
+
+# print(a); print(A)
+
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# myvar2 = "John"
+
+# print(myvar); print(my_var); print(_my_var); print(myVar); print(myvar2)
+
+# MyVariableName = "John"
+# my_variable_name = "John"
+
+# print(MyVariableName); print(my_variable_name)
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x); print(y), print(z)
+
+# x = y = z = "Orange"
+# print(x); print(y); print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x); print(y); print(z)
+
+# x = "Python is awesome"
+# print(x)
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x,y,z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+
+# x = 5
+# y = 10
+# print(x + y)
+
+# x = "awesome" #グローバル関数
+
+# def myfunc():
+#     print("Python is " + x)
+
+# myfunc()
+
+# x = "awesome" # グローバル変数
+
+# def myfunc():
+#     x = "fantastic" # ローカル変数
+#     print("Python is " + x)
+
+# myfunc()
+# print("Python is " + x)
+
+# def myfunc():
+#     global x
+#     x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+# x = "awesome"
+
+# def myfunc():
+#     global x
+#     x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+"""データ型"""
+# x = 1
+# y = 2.8
+# z = 1j
+
+# print(type(x)); print(type(y)); print(type(z))
+
+# x = 1
+# y = 35656222554887711
+# z = -3255522
+
+# print(type(x)); print(type(y)); print(type(z))
+
+# x = 1.10
+# y = 1.0
+# z = -35.59
+
+# print(type(x)); print(type(y)); print(type(z))
+
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+
+# print(type(x)); print(type(y)); print(type(z))
+
+# x = 3+5j
+# y = 5j
+# z = -5j
+
+# print(type(x)); print(type(y)); print(type(z))
+
+# x = 1
+# y = 2.8
+# z = 1j
+
+# a = float(x)
+# b = int(y)
+# c = complex(x)
+
+# print(a, b, c)
+# print(type(a)), print(type(b)), print(type(c))
+
+print(random.randrange(1, 10))
+
+
+
+
+
+
+

2c57f7130c890b9c5fbdb27c6712667d91eb5725
{yoshikaneichimura} <{yoshikanemobile@gmail.com}>
2026-09-22 17:08:08 +0900

    演算子 [Add]

--- python.py
+++ python.py
@@ -332,7 +332,165 @@ more than just one line
 # txt = "We are the so-called \"Vikings\" from the north."
 # print(txt)
 
+"""bool値"""
 
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
 
+# a = 200
+# b = 33
 
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than b")
 
+# print(bool("Hello"))
+# print(bool(15))
+
+# x = "Hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+# print(bool("abc"))
+# print(bool(123))
+# print(bool(["apple", "cherry", "banana"]))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+
+# class myclass():
+#     def __len__(self):
+#         return 0
+
+# myobj = myclass()
+# print(bool(myobj))
+
+# def myfunction() :
+#     return True
+# print(myfunction())
+
+# def myfunction():
+#     return True
+
+# if myfunction():
+#     print("YES!")
+# else:
+#     print("NO!")
+# x = 200
+
+# print(isinstance(x, int))
+
+"""演算子"""
+
+# print(10 + 5)
+
+# sum1 = 100 + 50
+# sum2 = sum1 + 150
+# sum3 = sum2 + sum2
+
+# print(sum1, sum2, sum3)
+
+# x = 15
+# y = 4
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+# numbers = [1,2,3,4,5]
+
+# if (count := len(numbers)) > 3:
+#     print(f"List has {count} elements")
+
+# num = 6
+
+# x = "WEEKEND!" if num > 5 else "Workday"
+
+# print(x)
+
+# num = 6
+
+# x = "Fri" if num == 5 else "Sat" if num  == 6 else "Sun" if num == 7 else "weekday"
+
+# print(x)
+
+# x = 5
+# y = 3
+
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+
+# x = 5
+
+# print(1 < x < 10)
+# print(1 < x and x < 10)
+
+# x = 5
+
+# print(x > 0 and x < 10)
+
+# x = 5
+
+# print(x < 5 or x > 10)
+
+# x = 5
+
+# print(not(x > 3 and x < 10 ))
+
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+
+# print(x is z)
+# print(x is y)
+# print(x == y)
+
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+
+# print(x is not y)
+
+# x = [1,2,3]
+# y = [1,2,3]
+
+# print(id(x)); print(id(y))
+# print(x == y); print(x is y)
+
+# fruits = ["apple", "banana", "cherry"]
+# print("banana" in fruits)
+
+# fruits = ["apple", "banana", "cherry"]
+
+# print("pinapple" not in fruits)
+
+# text = "Hello World"
+
+# print("H" in text); print("hello" in text); print("z" not in text)
+
+# print(6 & 3)
+
+# print(6 | 3)
+
+# print(6 ^ 3)
+
+# print((6 + 3) - (6 + 3))
+
+# print(100 + 5 * 3)
+
+# print(5 + 4 - 7 + 3)
